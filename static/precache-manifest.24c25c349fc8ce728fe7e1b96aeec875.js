1ba16cc3b2be0c9159c530d91b4e14f18eca7dd7
Leo Gonsalves <lcgonsalves@wpi.edu>
2019-11-01 20:36:29 +0000

    added static

---
+++ static/precache-manifest.24c25c349fc8ce728fe7e1b96aeec875.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9203d812ebf02f76e2f9fa46d282358c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4d27e424641045e77ce",
+    "url": "/static/css/main.46061eca.chunk.css"
+  },
+  {
+    "revision": "1ca28ba6f71dce08b17f",
+    "url": "/static/js/2.eb26e6f4.chunk.js"
+  },
+  {
+    "revision": "f4d27e424641045e77ce",
+    "url": "/static/js/main.2070d948.chunk.js"
+  },
+  {
+    "revision": "cbb19f815779577eec7d",
+    "url": "/static/js/runtime~main.6146ab18.js"
+  }
+]);
