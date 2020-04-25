fc89c650b7d9c1819952c6ed4cc1ec7a4581f83a
Leo Gonsalves <lcgonsalves@wpi.edu>
2020-04-25 19:37:36 -0400

    base url changed to api url

--- src/routes/ProxyRouter.js
+++ src/routes/ProxyRouter.js
@@ -2,8 +2,8 @@ const {Router} = require("express");
 const request = require("request");
 
 const router = Router();
-const temp = "https://api.leogons.com/tm";
-const baseUrl = "http://localhost:9595/tm";
+const baseUrl = "https://api.leogons.com/tm";
+const devUrl = "http://localhost:9595/tm";
 
 // proxies all requests to API server
 router.all("/*", (req, res) => {
