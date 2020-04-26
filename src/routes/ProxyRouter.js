26d59afab1e7b67f01e528597372852e339fa00e
Leo Gonsalves <lcgonsalves@wpi.edu>
2020-04-25 20:30:45 -0400

    fixed proxying issue that was causing authentication errors by passing all headers forward

--- src/routes/ProxyRouter.js
+++ src/routes/ProxyRouter.js
@@ -11,14 +11,32 @@ router.all("/*", (req, res) => {
     const options = {
         "url": baseUrl + req.path,
         "json": true,
-        "body": req.body,
-        "headers": req.headers
+        "body": req.body
     };
 
+    // pass on game version header if present
+    const gameVersion = req.header("x-game-version");
+    if (gameVersion) {
+        options.headers = {
+            "X-Game-Version": gameVersion
+        };
+    }
+
+    console.log("Proxying request to", options.url);
+    console.log(options);
+
     if (req.method === "GET") {
-        request.get(options, (error, response, body) => res.status(response.statusCode).send(body));
+        request.get(options, (error, response, body) => {
+            console.log("Response received.", body, error);
+            if (!error) res.status(response ? response.statusCode : 200).send(body);
+            else res.status(400).send(error);
+        });
     } else if (req.method === "POST") {
-        request.post(options, (error, response, body) => res.status(response.statusCode).send(body));
+        request.post(options, (error, response, body) => {
+            console.log("Response received.", body, error);
+            if (!error) res.status(response ? response.statusCode : 200).send(body);
+            else res.status(400).send(error);
+        });
     } else {
         res.status(404).send({
            "status": "failure",
